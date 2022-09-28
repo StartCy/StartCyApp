51f486c5f8b3f113e1ab22d9d28290cc44c3b154
Ninja <evangelou.e.95@gmail.com>
2022-09-28 16:02:58 +0300

    fix errors

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import Link from 'next/link'
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
     <>
